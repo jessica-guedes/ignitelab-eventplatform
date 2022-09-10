c3b6749cb5bc1bca8438e095ab615ff6e635abc3
Jéssica <jessicaguedes.ads@gmail.com>
2022-09-10 15:27:16 -0400

    update

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ApolloProvider } from '@apollo/client'
+import { client } from './lib/apollo'
+import { Router } from './Router'
+import { Event } from './pages/Event'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+   </ApolloProvider>
+  )
+}
+
+export default App
